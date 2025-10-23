478b6534683f410cc0dfa9a8098823ce7f7d8e5f
aki0625 <0wp092537290w7u@au.com>
2025-10-24 03:28:21 +0900

    set bgm

--- Assets/Script/UI_script/PageSelectUI.cs
+++ Assets/Script/UI_script/PageSelectUI.cs
@@ -3,20 +3,39 @@ using UnityEngine.SceneManagement;
 
 public class PageSelectUI : MonoBehaviour
 {
-    public void OnClickLogin() => SceneManager.LoadScene("StageSelectScene");
-    public void OnClickStageSelect() => SceneManager.LoadScene("StageSelectScene");
-    public void OnClickHome() => SceneManager.LoadScene("TitleScene");
+    [SerializeField] private AudioClip clickSE;
+    public void OnClickLogin()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+        SEManager.Instance.PlayOneShot(clickSE);
+    }
+
+    public void OnClickStageSelect()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+        SEManager.Instance.PlayOneShot(clickSE);
+    }
+
+    public void OnClickHome()
+    {
+        SceneManager.LoadScene("TitleScene");
+        SEManager.Instance.PlayOneShot(clickSE);
+    }
+
     public void OnClickRetry()
     {
         // GameManagerの現在ステージ取得を使う前提
         int stage = 1;
         if (GameManager.instance != null)
+        {
             stage = Mathf.Clamp(
                 (int)GameManager.instance
                       .GetType()
                       .GetMethod("GetCurrentStageNumber", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
                       .Invoke(GameManager.instance, null),
                 1, 12);
+            SEManager.Instance.PlayOneShot(clickSE);
+        }
 
         SceneManager.LoadScene($"stage{stage:00}");
     }
