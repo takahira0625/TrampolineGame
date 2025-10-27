d17540252ecbc86bfcdeb4118203f7cf4fab365b
OKADA Yudai <i520092z@mails.cc.ehime-u.ac.jp>
2025-10-27 16:26:31 +0900

    create BlockCatalog

--- Assets/Script/UI_script/Button/PageSelectUI.cs
+++ Assets/Script/UI_script/Button/PageSelectUI.cs
@@ -52,4 +52,13 @@ public class PageSelectUI : MonoBehaviour
 
         SceneManager.LoadScene($"Stage{stage:00}");
     }
+
+    public void OnClickCatalog01()
+    {
+        SceneManager.LoadScene("BlockCatalogScene01");
+    }
+
+    public void OnClickCatalog02() {
+        SceneManager.LoadScene("BlockCatalogScene02");
+    }
 }
