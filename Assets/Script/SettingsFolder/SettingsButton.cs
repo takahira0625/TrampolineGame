ab7cc0689b27e454edb9dbfba02c97a80bade7be
aki0625 <0wp092537290w7u@au.com>
2025-11-06 01:11:18 +0900

    set se UI

--- Assets/Script/SettingsFolder/SettingsButton.cs
+++ Assets/Script/SettingsFolder/SettingsButton.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class SettingsButton : MonoBehaviour
 {
     [SerializeField] private GameObject settingsPanel;
-
+    [SerializeField] private AudioClip clickSE;
     public void ToggleSettings()
     {
+        SEManager.Instance.PlayOneShot(clickSE);
         settingsPanel.SetActive(!settingsPanel.activeSelf);
     }
 }
